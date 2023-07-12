5675fad991615fe1f43e003259886c68539ac3ab
actualview <138713344+actualview@users.noreply.github.com>
2023-07-13 06:57:39 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+window.onload = () => {
+     let places = staticLoadPlaces();
+     renderPlaces(places);
+};
+
+function staticLoadPlaces() {
+    return [
+        {
+            name: 'Trex',
+            location: {
+                lat: 35.152130,
+                lng: 129.134997,
+            }
+        },
+    ];
+}
+
+function renderPlaces(places) {
+    let scene = document.querySelector('a-scene');
+
+    places.forEach((place) => {
+        let latitude = place.location.lat;
+        let longitude = place.location.lng;
+
+        let model = document.createElement('a-entity');
+        model.setAttribute('gps-entity-place', `latitude: ${35.152130}; longitude: ${129.134997};`);
+        model.setAttribute('gltf-model', './assets/trex/scene.gltf');
+        model.setAttribute('rotation', '0 5 0');
+        model.setAttribute('animation-mixer', '');
+        model.setAttribute('scale', '0.01 0.01 0.01');
+
+        model.addEventListener('loaded', () => {
+            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+        });
+
+        scene.appendChild(model);
+    });
+}
