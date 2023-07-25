035a6a800d3c4f046e0fd8db96401a792c8a4e44
actualview <138713344+actualview@users.noreply.github.com>
2023-07-25 21:59:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function renderPlaces(places) {
         let model = document.createElement('a-entity');
         model.setAttribute('gps-entity-place', `latitude: ${35.149027}; longitude: ${129.065187};`);
         model.setAttribute('gltf-model', './assets/whale/scene.gltf');
-        model.setAttribute('rotation', '0 90 0');
+        model.setAttribute('rotation', '0 30 0');
         model.setAttribute('animation-mixer', '');
         model.setAttribute('scale', '0.8 0.8 0.8');
 
